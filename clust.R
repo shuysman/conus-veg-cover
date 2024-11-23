eb1d371ccf775874b3873857d6ec953d8c00c530
Stephen Huysman <shuysman@gmail.com>
2024-11-22 18:57:17 -0700

    add scratch code

--- clust.R
+++ clust.R
@@ -1,8 +1,12 @@
 library(terra)
 library(mclust)
+library(cluster)
+library(tidyverse)
 
-aet_annual <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/AET/historical/V_1_5_annual_gridmet_historical_AET_2000_2019_annual_means_cropped_units_mm.tif")
-cwd_annual <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/Deficit/historical/V_1_5_annual_gridmet_historical_Deficit_2000_2019_annual_means_cropped_units_mm.tif")
+set.seed(255)
+
+aet_annual <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/AET/historical/V_1_5_annual_gridmet_historical_AET_1980_1999_annual_means_cropped_units_mm.tif")
+cwd_annual <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/Deficit/historical/V_1_5_annual_gridmet_historical_Deficit_1980_1999_annual_means_cropped_units_mm.tif")
 ## aet_winter <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/AET/historical/V_1_5_winter_gridmet_historical_AET_1980_1999_winter_means_cropped_units_mm.tif")
 ## cwd_winter <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/Deficit/historical/V_1_5_winter_gridmet_historical_Deficit_1980_1999_winter_means_cropped_units_mm.tif")
 ## aet_spring <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/AET/historical/V_1_5_spring_gridmet_historical_AET_1980_1999_spring_means_cropped_units_mm.tif")
@@ -12,10 +16,12 @@ cwd_annual <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/
 ## aet_fall <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/AET/historical/V_1_5_fall_gridmet_historical_AET_1980_1999_fall_means_cropped_units_mm.tif")
 ## cwd_fall <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/Deficit/historical/V_1_5_fall_gridmet_historical_Deficit_1980_1999_fall_means_cropped_units_mm.tif")
 
+## https://www.mrlc.gov/downloads/sciweb1/shared/mrlc/metadata/Annual_NLCD_LndCov_2023_CU_C1V0.xml
+nlcd <- rast("./nlcd_resampled_1km_1985.tif")
 
-stack <- c(aet_annual, cwd_annual)
+stack <- c(aet_annual, cwd_annual, nlcd)
 
-names(stack) <- c("aet_annual", "cwd_annual")
+names(stack) <- c("aet_annual", "cwd_annual", "nlcd")
 
 ##stack <- c(aet_winter, cwd_winter,
            ## aet_spring, cwd_spring,
@@ -31,7 +37,20 @@ names(stack) <- c("aet_annual", "cwd_annual")
 
 stack_df <- as.data.frame(stack)
 
+stack_df$nlcd <- factor(stack_df$nlcd)
+
 head(stack_df)
+
+summarize(stack_df, aet = mean(aet_annual, na.rm = TRUE), cwd = mean(cwd_annual, na.rm = TRUE), .by = nlcd)
+
+ggplot(data = stack_df) +
+  geom_boxplot(aes(y = cwd_annual, x = nlcd))
+
+ggplot(data = stack_df) +
+  geom_boxplot(aes(y = aet_annual, x = nlcd))
+
+ggplot(data = stack_df, mapping = aes(x = cwd_annual, y = aet_annual)) +
+  stat_density_2d(aes(fill = ..level..), geom = "polygon", colour="white")
 
 subset <- sample(1:nrow(stack_df), 10000)
 BIC <- mclustBIC(stack_df, initialization = list(subset = subset))
@@ -56,3 +75,13 @@ vec[complete.cases(as.data.frame(aet_annual, na.rm = FALSE))] = mod1$classificat
 clustering = rast(aet_annual, nlyrs = 1, vals = vec) 
 
 plot(clustering)
+
+idx = sample(1:nrow(stack_df), size = 10000)
+head(idx)
+
+sil = silhouette(mod1$classification[idx], dist(stack_df[idx, ]))
+
+summary(sil)
+
+colors = rainbow(n = 6)
+plot(sil, border = NA, col = colors, main = "Silhouette Index")
