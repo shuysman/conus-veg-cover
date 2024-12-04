855031ed30ee0e4b0aed20692d41abd419321c5e
Stephen Huysman <shuysman@gmail.com>
2024-12-03 20:13:58 -0700

    update paths for tempest

--- randomforest.R
+++ randomforest.R
@@ -4,35 +4,40 @@ library(tidyverse)
 
 set.seed(255)
 
-aet_winter <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/AET/historical/V_1_5_winter_gridmet_historical_AET_2000_2019_winter_means_cropped_units_mm.tif")
-cwd_winter <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/Deficit/historical/V_1_5_winter_gridmet_historical_Deficit_2000_2019_winter_means_cropped_units_mm.tif")
-aet_spring <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/AET/historical/V_1_5_spring_gridmet_historical_AET_2000_2019_spring_means_cropped_units_mm.tif")
-cwd_spring <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/Deficit/historical/V_1_5_spring_gridmet_historical_Deficit_2000_2019_spring_means_cropped_units_mm.tif")
-aet_summer <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/AET/historical/V_1_5_summer_gridmet_historical_AET_2000_2019_summer_means_cropped_units_mm.tif")
-cwd_summer <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/Deficit/historical/V_1_5_summer_gridmet_historical_Deficit_2000_2019_summer_means_cropped_units_mm.tif")
-aet_fall <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/AET/historical/V_1_5_fall_gridmet_historical_AET_2000_2019_fall_means_cropped_units_mm.tif")
-cwd_fall <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/Deficit/historical/V_1_5_fall_gridmet_historical_Deficit_2000_2019_fall_means_cropped_units_mm.tif")
+aet_winter <- rast("./data/summary_layers/AET/historical/V_1_5_winter_gridmet_historical_AET_2000_2019_winter_means_cropped_units_mm.tif")
+cwd_winter <- rast("./data/summary_layers/Deficit/historical/V_1_5_winter_gridmet_historical_Deficit_2000_2019_winter_means_cropped_units_mm.tif")
+aet_spring <- rast("./data/summary_layers/AET/historical/V_1_5_spring_gridmet_historical_AET_2000_2019_spring_means_cropped_units_mm.tif")
+cwd_spring <- rast("./data/summary_layers/Deficit/historical/V_1_5_spring_gridmet_historical_Deficit_2000_2019_spring_means_cropped_units_mm.tif")
+aet_summer <- rast("./data/summary_layers/AET/historical/V_1_5_summer_gridmet_historical_AET_2000_2019_summer_means_cropped_units_mm.tif")
+cwd_summer <- rast("./data/summary_layers/Deficit/historical/V_1_5_summer_gridmet_historical_Deficit_2000_2019_summer_means_cropped_units_mm.tif")
+aet_fall <- rast("./data/summary_layers/AET/historical/V_1_5_fall_gridmet_historical_AET_2000_2019_fall_means_cropped_units_mm.tif")
+cwd_fall <- rast("./data/summary_layers/Deficit/historical/V_1_5_fall_gridmet_historical_Deficit_2000_2019_fall_means_cropped_units_mm.tif")
 
-accumswe <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/accumswe/historical/V_1_5_annual_gridmet_historical_accumswe_2000_2019_annual_means_cropped_units_mm.tif")
+pet_winter <- rast("./data/summary_layers/PET/historical/V_1_5_winter_gridmet_historical_PET_2000_2019_winter_means_cropped_units_mm.tif")
+pet_spring <- rast("./data/summary_layers/PET/historical/V_1_5_spring_gridmet_historical_PET_2000_2019_spring_means_cropped_units_mm.tif")
+pet_summer <- rast("./data/summary_layers/PET/historical/V_1_5_summer_gridmet_historical_PET_2000_2019_summer_means_cropped_units_mm.tif")
+pet_fall <- rast("./data/summary_layers/PET/historical/V_1_5_fall_gridmet_historical_PET_2000_2019_fall_means_cropped_units_mm.tif")
 
-rain_winter <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/rain/historical/V_1_5_annual_gridmet_historical_rain_2000_2019_annual_means_cropped_units_mm.tif")
-rain_spring <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/rain/historical/V_1_5_spring_gridmet_historical_rain_2000_2019_spring_means_cropped_units_mm.tif")
-rain_summer <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/rain/historical/V_1_5_summer_gridmet_historical_rain_2000_2019_summer_means_cropped_units_mm.tif")
-rain_fall <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/rain/historical/V_1_5_fall_gridmet_historical_rain_2000_2019_fall_means_cropped_units_mm.tif")
+accumswe <- rast("./data/summary_layers/accumswe/historical/V_1_5_annual_gridmet_historical_accumswe_2000_2019_annual_means_cropped_units_mm.tif")
 
-runoff_winter <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/runoff/historical/V_1_5_annual_gridmet_historical_runoff_2000_2019_annual_means_cropped_units_mm.tif")
-runoff_spring <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/runoff/historical/V_1_5_spring_gridmet_historical_runoff_2000_2019_spring_means_cropped_units_mm.tif")
-runoff_summer <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/runoff/historical/V_1_5_summer_gridmet_historical_runoff_2000_2019_summer_means_cropped_units_mm.tif")
-runoff_fall <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/runoff/historical/V_1_5_fall_gridmet_historical_runoff_2000_2019_fall_means_cropped_units_mm.tif")
+rain_winter <- rast("./data/summary_layers/rain/historical/V_1_5_annual_gridmet_historical_rain_2000_2019_annual_means_cropped_units_mm.tif")
+rain_spring <- rast("./data/summary_layers/rain/historical/V_1_5_spring_gridmet_historical_rain_2000_2019_spring_means_cropped_units_mm.tif")
+rain_summer <- rast("./data/summary_layers/rain/historical/V_1_5_summer_gridmet_historical_rain_2000_2019_summer_means_cropped_units_mm.tif")
+rain_fall <- rast("./data/summary_layers/rain/historical/V_1_5_fall_gridmet_historical_rain_2000_2019_fall_means_cropped_units_mm.tif")
 
-soil_water_winter <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/soil_water/historical/V_1_5_annual_gridmet_historical_soil_water_2000_2019_annual_means_cropped_units_mm.tif")
-soil_water_spring <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/soil_water/historical/V_1_5_spring_gridmet_historical_soil_water_2000_2019_spring_means_cropped_units_mm.tif")
-soil_water_summer <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/soil_water/historical/V_1_5_summer_gridmet_historical_soil_water_2000_2019_summer_means_cropped_units_mm.tif")
-soil_water_fall <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/soil_water/historical/V_1_5_fall_gridmet_historical_soil_water_2000_2019_fall_means_cropped_units_mm.tif")
+runoff_winter <- rast("./data/summary_layers/runoff/historical/V_1_5_annual_gridmet_historical_runoff_2000_2019_annual_means_cropped_units_mm.tif")
+runoff_spring <- rast("./data/summary_layers/runoff/historical/V_1_5_spring_gridmet_historical_runoff_2000_2019_spring_means_cropped_units_mm.tif")
+runoff_summer <- rast("./data/summary_layers/runoff/historical/V_1_5_summer_gridmet_historical_runoff_2000_2019_summer_means_cropped_units_mm.tif")
+runoff_fall <- rast("./data/summary_layers/runoff/historical/V_1_5_fall_gridmet_historical_runoff_2000_2019_fall_means_cropped_units_mm.tif")
 
-soil_whc <- rast("./soil_whc_conus_1km.tif")
+soil_water_winter <- rast("./data/summary_layers/soil_water/historical/V_1_5_annual_gridmet_historical_soil_water_2000_2019_annual_means_cropped_units_mm.tif")
+soil_water_spring <- rast("./data/summary_layers/soil_water/historical/V_1_5_spring_gridmet_historical_soil_water_2000_2019_spring_means_cropped_units_mm.tif")
+soil_water_summer <- rast("./data/summary_layers/soil_water/historical/V_1_5_summer_gridmet_historical_soil_water_2000_2019_summer_means_cropped_units_mm.tif")
+soil_water_fall <- rast("./data/summary_layers/soil_water/historical/V_1_5_fall_gridmet_historical_soil_water_2000_2019_fall_means_cropped_units_mm.tif")
 
-nlcd <- rast("./nlcd_resampled_1km_2019.tif")
+soil_whc <- rast(".data/soil_whc_conus_1km.tif")
+
+nlcd <- rast("./data/nlcd_resampled_1km_2019.tif")
 
 nlcd <- nlcd %>%
   subst(c(21, 22, 23, 24, 81, 82, 11), NA, raw = TRUE) ### Remove cultivated land types, developed land, open water
