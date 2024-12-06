5c7cae667d6217d64b3515307f373a5ced38bdf1
Stephen Huysman <shuysman@gmail.com>
2024-12-05 22:25:40 -0700

    update plot

--- viz_veg_dist.R
+++ viz_veg_dist.R
@@ -8,10 +8,13 @@ library(ggalt)
 
 set.seed(255)
 
-aet_annual <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/AET/historical/V_1_5_annual_gridmet_historical_AET_2000_2019_annual_means_cropped_units_mm.tif")
-cwd_annual <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/Deficit/historical/V_1_5_annual_gridmet_historical_Deficit_2000_2019_annual_means_cropped_units_mm.tif")
 
-nlcd <- rast("./data/nlcd_resampled_1km_2019.tif")
+
+nlcd <- rast("./data/nlcd_resampled_1km_2019.tif") %>%
+  filter(! values(.) %in% c(11, 21, 22, 23, 24, 81, 82))
+
+aet_annual <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/AET/historical/V_1_5_annual_gridmet_historical_AET_2000_2019_annual_means_cropped_units_mm.tif") %>% mask(nlcd)
+cwd_annual <- rast("/media/smithers/shuysman/data/nps_gridded_wb/summary_layers/Deficit/historical/V_1_5_annual_gridmet_historical_Deficit_2000_2019_annual_means_cropped_units_mm.tif") %>% mask(nlcd)
 
 stack <- c(aet_annual, cwd_annual, nlcd)
 
@@ -38,18 +41,23 @@ ggplot(data = stack_df, mapping = aes(x = cwd_annual, y = aet_annual, fill = nlc
 #### 
 plot_func <- function(df, name) {
   ggplot(data = df, aes(x = cwd_annual, y = aet_annual)) +
-    geom_bin2d(bins = 64) +
+    geom_bin2d(bins = 32) +
     scale_fill_viridis_c(limits = c(1000, NA)) +
     xlim(0, 1500) +
     ylim(0, 1500) +
     ggtitle(name) +
-    guides(fill="none")
-  }
+    xlab("Annual CWD") +
+    ylab("Annual AET") +
+    guides(fill = "none") +
+    theme_bw() +
+    theme(plot.title = element_text(size = 14))
+}
 
 nested_stack <- stack_df %>% 
   group_by(nlcd) %>% 
   nest() %>% 
   mutate(plots = map2(data, nlcd, plot_func)) 
 
-gridExtra::grid.arrange(grobs = nested_stack$plots, top = "Density of pixels of 1985 Cover Type by 1980-1999 Annual AET/CWD")
+plt <- gridExtra::grid.arrange(grobs = nested_stack$plots, top = "Density of pixels vs '00-'19 Annual AET/CWD")
+ggsave("img/projected_cover_change.png", plot = plt, width = 10, height = 10)
 ####
